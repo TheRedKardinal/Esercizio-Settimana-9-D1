cb585eaa57273d14d32dd8e8eb1467988849ba98
TheRedKardinal <claudio_giasi@icloud.com>
2026-06-30 16:49:33 +0200

    Lista renderizzata

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import NavbarRender from "./assets/componets/NavBar";
 import HeroRender from "./assets/componets/Hero";
+import ListRender from "./assets/componets/List";
 
 const App = function () {
   return (
     <>
-      <NavbarRender />
-      <HeroRender />
+      <header>
+        <NavbarRender />
+      </header>
+      <main>
+        <HeroRender />
+        <ListRender />
+      </main>
     </>
   );
 };
--- src/assets/componets/List.jsx
+++ src/assets/componets/List.jsx
@@ -1,1 +1,17 @@
 import books from "../dati/fantasy.json";
+import { Row, Col } from "react-bootstrap";
+
+function ListRender() {
+  return (
+    <Row>
+      {books.map((book) => (
+        <Col xs={12} md={4}>
+          <img src={book.img} />
+          <h2>{book.title}</h2>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+export default ListRender;
